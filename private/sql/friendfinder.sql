37afdc685cbd7d10dc5a7f821d665344690dcd79
Luecu <luca.indermuehle@gmail.com>
2016-06-14 11:56:43 +0200

    LI - add living place - improve date presentation

--- private/sql/friendfinder.sql
+++ private/sql/friendfinder.sql
@@ -58,7 +58,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `firstname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `livingplace`varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
